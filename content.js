53a54ce26f2d83af55946ba5e19fb00382bc801b
Talia Makarova <nvm6nf@virginia.edu>
2021-11-06 13:02:10 -0400

    Simple Popup

--- content.js
+++ content.js
@@ -1,11 +1,28 @@
+/*
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.message === 'clicked_browser_action') {
             var firstHref = $("a[href^='http']").eq(0).attr("href");
 
             console.log(firstHref);
+
+            // send url back
+            chrome.runtime.sendMessage({"message": "open_new_tab", "url":firstHref});
         }
     }
-)
+);
+*/
+// simple popup with text:
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('clickIt')
+    checkPageButton.addEventListener('home', function() {
+        chrome.tabs.getSelected(null, function(tab) {
+            window.onload=function() {setTimeout(showPopup, 7000)}
+            function showPopup() {
+                alert("Hello! This is Timed Sloth");
+            }
+        });
 
+    }, false);
+}, false);
 
