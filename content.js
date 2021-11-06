fcf30a70d22473bc1b5f6bac43080e2b34d466ef
Talia Makarova <nvm6nf@virginia.edu>
2021-11-06 14:42:52 -0400

    timer with selected option

--- content.js
+++ content.js
@@ -4,7 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
     checkTimerButton.addEventListener('click', function() {
     // null - don't need event
     // timed alert
-        time = document.getElementById('time-options')
-        setTimeout(alert("Hey! You've been working hard! Time to take a 2 minute break."),time);
+        var time = document.getElementById('time-options')
+        // get selected option
+        var val = time.options[time.selectedIndex].value;
+        var text = time.options[time.selectedIndex].text;
+        time_int = text.replace(/\D/g,'');
+        // hours 
+        if (time_int < 3) {
+            milliseconds = time_int * 3600000
+        }
+        // minuts 
+        else {
+            milliseconds = time_int * 60000
+        }
+
+        setTimeout(alert("Hey! You've been working hard! Time to take a 2 minute break."), milliseconds);
+        
     }, false);
 }, false);
